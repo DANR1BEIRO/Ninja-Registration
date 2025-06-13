2a6abf93cec9a7dde18db436ba80c6a275970cef
Daniel Ribeiro <danribeiro001@gmail.com>
2025-06-13 12:01:09 -0300

    Criado a Controller

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,21 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")  // Mapeia requisições HTTP GET. Usado para buscar/ler informações do servidor.
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
+
+
+/*
+@PostMapping // Mapeia requisições HTTP POST. Usado para enviar/criar novos recursos no servidor.
+@PutMapping // Mapeia requisições HTTP PUT. Usado para substituir/atualizar um recurso inteiro no servidor.
+@PatchMapping // Mapeia requisições HTTP PATCH. Usado para atualizações parciais de um recurso.
+@DeleteMapping *///  Mapeia requisições HTTP DELETE. Usado para remover um recurso do servidor.
