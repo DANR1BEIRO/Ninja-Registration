6cdefec124ea8d0a4807665784b73ffd8e81e3e6
Daniel Ribeiro <danribeiro001@gmail.com>
2025-06-15 14:26:20 -0300

    feat: Cria o MissionsRepository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionsRepository.java
@@ -0,0 +1,7 @@
+package dev.java10x.CadastroDeNinjas.Missions;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissionsRepository extends JpaRepository<MissionModel, Long> {
+
+}
