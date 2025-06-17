c286fd4738312e3dacf88dac3d91354aea65324e
Daniel Ribeiro <danribeiro001@gmail.com>
2025-06-17 18:02:24 -0300

    feat: Adiciona métodos no MissionService para listar missões e buscar por ID

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missions/MissionService.java
@@ -0,0 +1,27 @@
+package dev.java10x.CadastroDeNinjas.Missions;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MissionService {
+
+    private MissionsRepository missionsRepository;
+
+    public MissionService(MissionsRepository missionsRepository) {
+        this.missionsRepository = missionsRepository;
+    }
+
+    public List<MissionModel> displayAllMissions() {
+        return missionsRepository.findAll();
+    }
+
+    public Optional<MissionModel> getMissionById(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Mission can't be null");
+        }
+        return missionsRepository.findById(id);
+    }
+}
